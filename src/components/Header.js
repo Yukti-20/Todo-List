c4c5b212d0d43a6a6387a802dc60f0d8c2e98507
Yukti Vivek <aryayuvi1101@gmail.com>
2022-05-14 16:33:42 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+//responsible to display the name of app
+const Header = () => {
+  return (
+    <div className="header">
+        <h1>Todos</h1>
+    </div>
+  )
+}
+
+export default Header;
